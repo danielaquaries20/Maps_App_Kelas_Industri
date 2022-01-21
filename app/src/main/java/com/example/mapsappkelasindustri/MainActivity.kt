c58aee370822389e58e752bfebcab0363b5884a4
Daniel Pratama <danielaquaries@gmail.com>
2022-01-21 17:19:11 +0700

    Menambahkan Autocomplete.

--- app/src/main/java/com/example/mapsappkelasindustri/MainActivity.kt
+++ app/src/main/java/com/example/mapsappkelasindustri/MainActivity.kt
@@ -6,9 +6,14 @@ import android.util.Log
 import com.crocodic.core.base.activity.NoViewModelActivity
 import com.crocodic.core.extension.checkLocationPermission
 import com.example.mapsappkelasindustri.databinding.ActivityMainBinding
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 class MainActivity : NoViewModelActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +21,34 @@ class MainActivity : NoViewModelActivity<ActivityMainBinding>() {
         setLayoutRes(R.layout.activity_main)
         binding.mapView.onCreate(savedInstanceState)
 
+        //Initialize the SDK
+        Places.initialize(applicationContext, "AIzaSyCLdFcogaVqXIrBQnAl4229PVcbg7zp-SE")
+
+        //Initialize the AutocompleteSupportFragment
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentAutocomplete) as AutocompleteSupportFragment
+
+        //Specify the types of place data to return
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        //Set up a PlaceSelectionListener to handle the response
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onError(status: Status) {
+                TODO("Not yet implemented")
+                Log.i("latihan_autocomple", "An error occurred: $status")
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                TODO("Not yet implemented")
+                Log.i("latihan_autocomple", "Place: ${place.name}, ${place.id}")
+            }
+
+        })
+
+
         checkLocationPermission {
             listenLocationChange()
         }
-
 
     }
 
